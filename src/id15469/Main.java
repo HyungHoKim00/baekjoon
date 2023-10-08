332203b2cb157f21092099c8147e7d40973090fe
HyungHoKim <hkim1109@naver.com>
2023-10-08 23:40:19 +0900

    solved

--- src/id15469/Main.java
+++ src/id15469/Main.java
@@ -10,17 +10,13 @@ public class Main {
         StringTokenizer st = new StringTokenizer(s);
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n+1];
-        for(int i = 1; i<n+1;i++){
-            arr[i] = i;
-        }
         int[] output = new int[m];
         boolean[] visited = new boolean[n+1];
 
-        printSequence(n,m,0, arr, output, visited);
+        printSequence(n,m,0,output, visited);
     }
 
-    public static void printSequence(int n, int m, int depth, int[] arr, int[] output, boolean[] v) throws IOException { //n:largest, m:length
+    public static void printSequence(int n, int m, int depth, int[] output, boolean[] v) throws IOException { //n:largest, m:length
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         if(depth == m){
             for(int i = 0; i<m;i++){
@@ -34,7 +30,7 @@ public class Main {
                 if(!v[i]){
                     v[i] = true;
                     output[depth] = i;
-                    printSequence(n,m,depth+1,arr,output,v);
+                    printSequence(n,m,depth+1,output,v);
                     v[i] = false;
                 }
             }
